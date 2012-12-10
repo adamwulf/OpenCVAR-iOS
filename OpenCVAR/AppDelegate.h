d7fff6f0c67636abf90daeee8fe0b9c2f2efd197
Joshua Newnham <josh@MacPro.local>
2012-12-10 21:07:17 +0000

    Initial Commit

---
+++ OpenCVAR/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  OpenCVAR
+//
+//  Created by Joshua Newnham on 10/12/2012.
+//  Copyright (c) 2012 We Make Play. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ OpenCVAR/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OpenCVAR
+//
+//  Created by Joshua Newnham on 10/12/2012.
+//  Copyright (c) 2012 We Make Play. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
